65c1cba86acde4f7514978a345d90eaa620f14f2
rodri <rodri@DESKTOP-H4MTBLU>
2020-10-10 14:00:31 -0300

    Adicionando o tabuleiro, colocando algumas peças, e testando as peças no tabuleiro

---
+++ Chess System/src/chess/ChessMatch.java
@@ -0,0 +1,34 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
+
+public class ChessMatch {
+	
+	private Board board;
+	
+	public ChessMatch() {
+		board = new Board(8,8);
+		initialSetup();
+	}
+	
+	public ChessPiece[][] getPieces(){
+		ChessPiece[][] matriz = new ChessPiece[board.getColumns()][board.getRows()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				matriz[i][j] =  (ChessPiece) board.piece(i, j);
+			}
+		}
+		return matriz;
+	}
+	
+	private void initialSetup(){
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+
+	}
+
+}
